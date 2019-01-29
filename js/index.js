2d63ee3b9c717c392ddd12d8680c78d98ebf9e45
Md Kawsar Hussen <mdkawsarhussen@Mds-MacBook-Pro.local>
2019-01-29 18:14:42 -0500

    Most stuffs are done

--- js/index.js
+++ js/index.js
@@ -1,1 +1,37 @@
 // Your code goes here
+logoHeading = document.querySelector('.logo-heading');
+//  This two event listeners work together to change the
+//  color of the header and make it bigger when hover over
+//  and getting back to its original state once hover out
+
+
+logoHeading.addEventListener('mouseover', e => e.target.classList.add('logoHeading'));
+logoHeading.addEventListener('mouseleave', e => e.target.classList.remove('logoHeading'));
+
+navBar = document.querySelector('.nav');
+//  Rotates the entire nav bar at keydown
+navBar.addEventListener('keydown', function(){
+    this.classList.add('navBar');
+    console.log('navBar');
+})
+navItems = document.querySelectorAll('.nav-link');
+navItems.forEach(item => {
+    item.addEventListener('dblclick', e => {e.target.style.color = 'Blue';})
+})
+
+introH2 = document.querySelector('.intro h2');
+
+introH2.addEventListener('drag', function(){
+    this.classList.add('introH2');
+    preventDefault();
+})
+
+allParagraphs = document.querySelectorAll('p');
+
+const homeButton = document.querySelector('.nav-link.home')
+homeButton.addEventListener('click', e => {
+    alert(`Home button fired a click event!`);
+    // ALL about stopping events from bubbling
+    //e.stopPropagation();
+  });
+  
